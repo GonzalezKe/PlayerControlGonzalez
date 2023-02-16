a2cbeeba62157c78c96fd6af457b3fa753a7a7c7
GonzalezKe <710293@student.dusd.net>
2023-02-16 09:56:26 -0800

    Finished the car
    Finished the car scene now working on the Plane challenge scene

--- PlayerControlGonzalez/Assets/Course Library/Scripts/PlayerController.cs
+++ PlayerControlGonzalez/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Private Variables
+    private float speed = 5.0f;
+    private float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+     
 
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // The Car Moves forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicled
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
